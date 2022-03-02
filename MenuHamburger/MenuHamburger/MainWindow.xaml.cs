b056f7da2583e740666e3ec2c0ce1267c2e2ad9d
Stasia <83281012+dekeyofspace@users.noreply.github.com>
2022-03-02 16:21:05 +0300

    Add files via upload

---
+++ MenuHamburger/MenuHamburger/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuHamburger
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+
+		private void ChangeWindowState() {
+			if (WindowState == WindowState.Normal)
+			{
+				this.WindowState = WindowState.Maximized;
+				Window_Edge.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				if (WindowState == WindowState.Maximized)
+				{
+					this.WindowState = WindowState.Normal;
+					Window_Edge.Visibility = Visibility.Visible;
+				}
+			}
+		}
+
+		private void Main_Window_Panel_MouseDown(object sender, MouseButtonEventArgs e) {
+			if (e.ChangedButton == MouseButton.Left && e.ClickCount ==2)
+			{
+				ChangeWindowState();
+			}
+		}
+	}
+}
